16ed42de036108aac60c5e8b5e2e0b9b9c4aa25b
Frederic Metrich <frederic.metrich@siemens.com>
2020-04-30 08:41:55 +0200

    EnvoyS integration in Emoncms

---
+++ EnvoyS2Emoncms.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+
+# This utility send a EnvoyS (Enphase) data to emoncms
+#  
+# coded by:
+# author : Frederic Metrich
+# Email : Frederic.Metrich@Live.COM 
+version = "v2.00"
+
+# if errors during executing this scrip make sure you installed phyton and the required modules/libraries
+import configparser
+import datetime
+import logging
+import json
+import urllib.request
+import time
+
+DataJson_all  = { }
+
+LogFile              = "/var/log/EnvoyS2Emoncms.log"
+LogFileLastMessage   = "/tmp/EnvoyS2Emoncms.log"
+WatchdogFile         = "/tmp/EnvoyS2Emoncms_Watchdog"
+
+# Set logging params
+logging.basicConfig(filename=LogFile,format='%(asctime)s PID(%(process)d) %(levelname)s: %(message)s',level=logging.DEBUG)
+
+###############################################################################################################
+# Procedures 
+###############################################################################################################
+
+logging.info("****** Starting EnvoyS2Emoncms.py ******")
+
+Config = configparser.ConfigParser()
+Config.read("/etc/Enphase/EnvoyS2Emoncms.cfg")
+
+emon_privateKey = Config['emoncms']['privatekey'] 
+emon_node_all   = Config['emoncms']['node_all'] 
+emon_host       = Config['emoncms']['host'] 
+emon_protocol   = Config['emoncms']['protocol'] 
+emon_url        = Config['emoncms']['url'] 
+
+envoy_url_all   = Config['envoy']['url_all'] 
+envoy_host      = Config['envoy']['host'] 
+envoy_protocol  = Config['envoy']['protocol'] 
+
+logging.info('Configuration settings:')
+logging.info('emoncms/privatekey: ' + emon_privateKey)
+logging.info('emoncms/node_all: ' + emon_node_all)
+logging.info('emoncms/host: ' + emon_host)
+logging.info('emoncms/protocol: ' + emon_protocol)
+logging.info('emoncms/url: ' + emon_url)
+
+logging.info('envoy/url_all: ' + envoy_url_all)
+logging.info('envoy/host: ' + envoy_host)
+logging.info('envoy/protocol: ' + envoy_protocol)
+logging.info('***********************')
+
+###############################################################################################################
+# Main program
+###############################################################################################################
+
+# Construct urls 
+url_envoy_all  = envoy_protocol + envoy_host + envoy_url_all
+envoy_keys = ['wNow', 'whLifetime']
+
+def ProceedData():
+  try:
+    # Fetch page with ENVOY detailed data
+    the_page_all = urllib.request.urlopen(url_envoy_all, timeout = 5).read()
+
+    if len(the_page_all) < 4300:
+      logging.warning("Page size: " + str(len(the_page_all)) + " \'" + str(the_page_all) + "\'")
+    else:
+      logging.info("Page size: " + str(len(the_page_all)))
+  except ValueError as err:
+    logging.error('Caught http exception: ' + str(err))
+    logging.error('Received data: \'' + str(the_page_all) + '\'')
+    return
+
+  # Write raw output from envoy to file
+  f1=open(LogFileLastMessage, "w")
+  f1.write("INFO: page length(" + str(len(the_page_all)) + "): \'" + str(the_page_all) + "\'")
+  f1.close()
+
+  try:
+    data_all = json.loads(the_page_all)
+  except ValueError as err:
+    logging.error('Caught json exception: ' + str(err))
+    logging.error('Received data: \'' + str(the_page_all) + '\'')
+    return
+
+  DataJson_all.clear() 
+
+  # "measurementType": "production" 
+  DataJson_all['prod_' + 'whToday'] = data_all['production'][1]['whToday']
+  for data in range(len(envoy_keys)):
+    DataJson_all['prod_' + envoy_keys[data]] = data_all['production'][1][envoy_keys[data]]
+  # for 3-phase details, comment out if you don't need it
+  for phase in range(3):
+    for data in range(len(envoy_keys)):
+      DataJson_all['prod_L' + str(phase + 1) + '_' + envoy_keys[data]] = data_all['production'][1]['lines'][phase][envoy_keys[data]]
+  
+  # "measurementType": "total-consumption"
+  DataJson_all['cons_' + 'whToday'] = data_all['consumption'][0]['whToday']
+  for data in range(len(envoy_keys)):
+    DataJson_all['cons_' + envoy_keys[data]] = data_all['consumption'][0][envoy_keys[data]]
+  # for 3-phase details, comment out if you don't need it
+  for phase in range(3):
+    for data in range(len(envoy_keys)):
+      DataJson_all['cons_L' + str(phase + 1) + '_' + envoy_keys[data]] = data_all['consumption'][0]['lines'][phase][envoy_keys[data]]
+
+  # "measurementType": "net-consumption"
+  # The next field is always zero, so skip it
+  # DataJson_all['cons_' + 'whToday'] = data_all['consumption'][1]['whToday']
+  for data in range(len(envoy_keys)):
+    DataJson_all['net_' + envoy_keys[data]] = data_all['consumption'][1][envoy_keys[data]]
+  # for 3-phase details, comment out if you don't need it
+  for phase in range(3):
+    for data in range(len(envoy_keys)):
+      DataJson_all['net_L' + str(phase + 1) + '_' + envoy_keys[data]] = data_all['consumption'][1]['lines'][phase][envoy_keys[data]]
+  
+  url_all = emon_protocol + emon_host + emon_url + "time=" + str(data_all['production'][1]['readingTime']) + "&node=" + emon_node_all + "&apikey=" + emon_privateKey + "&json=" + str( json.dumps(DataJson_all, separators=(',', ':')))
+
+  HTTPresult_all = urllib.request.urlopen(url_all)
+  if HTTPresult_all.getcode() == 200:
+    logging.info("Response code : " +  str(HTTPresult_all.getcode()))
+  else:
+    logging.error("Response code : " +  str(HTTPresult_all.getcode()))
+    logging.error("Original url was: '" + url_all + "'")
+
+# Do forever ....
+while True:
+  try:
+    # Write time to watchdog file, as a sign of life  
+    f3=open(WatchdogFile, "w")
+    timestamp = int(time.time())
+    f3.write (str(timestamp))
+    f3.close()
+
+    ProceedData()
+
+    time.sleep(5)
+
+  except Exception as e:
+    logging.error('Caught exception: ' + str(e))  
--- README.md
+++ README.md
@@ -1,1 +1,95 @@
 # EnvoyS2Emoncms
+
+Purpose of these files is a full integration of the Enphase EnvoyS-Metered multiphase in Emoncms.
+
+This includes:
+- configuration file for the EnvoyS and Emoncms settings
+- python script to read the EnvoyS data and send them to Emoncms
+- create of all feeds in Emoncms
+
+It has been tested with:
+- an **Envoy-S-Metered-EU** multiphase with firmware **5.0.34**.
+- emoncms **v10.2.1** installed locally on a Debian **10.3** x64.
+
+## Prerequisites
+
+### Emoncms
+Please see https://emoncms.org/ for details
+
+### Python3
+On Debian based system:
+```sh
+# Install python3
+apt-get install python3
+apt-get -y install python3-pip
+
+# Update pip
+pip3 install --upgrade pip
+```
+
+## Install on Debian based systems
+
+### Clone the repository ###
+Navigate to some location like your home directory or */var/tmp*
+```sh
+# Clone the repository
+git clone -b master https://github.com/FredM67/EnvoyS2Emoncms.git
+```
+### Scripts ###
+```sh
+cd EnvoyS2Emoncms
+sudo cp EnvoyS2Emoncms.py /usr/local/bin/EnvoyS2Emoncms.py
+sudo chmod +x /usr/local/bin/EnvoyS2Emoncms.py
+
+sudo cp EnvoyS2EmoncmsWatchdog.sh /usr/local/bin/EnvoyS2EmoncmsWatchdog.sh
+sudo chmod +x /usr/local/bin/EnvoyS2EmoncmsWatchdog.sh
+```
+
+### Configuration file ###
+Install the configuration file, rename it and modify it accordingly to your system by setting the fields marked with **< ... >**
+```sh
+sudo mkdir /etc/Enphase
+sudo cp EnvoyS2Emoncms_default.cfg /etc/Enphase/EnvoyS2Emoncms.cfg
+```
+
+At this point, if you want to check if everything is working properly, you can run the script by hand like this:
+```sh
+sudo /usr/local/bin/EnvoyS2Emoncms.py
+```
+You should see new inputs in your emoncms
+
+### Some system stuff ###
+Configure log rotation
+```sh
+sudo cp EnvoyS2Emoncms.logrotate /etc/logrotate.d/EnvoyS2Emoncms
+```
+
+Add a crontab for the watchdog
+```sh
+sudo crontab -e
+```
+add
+```sh
+* * * * *       /usr/local/bin/EnvoyS2EmoncmsWatchdog.sh
+```
+
+After that, the system will start automatically the python script within 1-2 minutes.
+
+In your emoncms, you should see new inputs under the node id set in the config file.
+
+## Emoncms templates ##
+For those with a local emoncms, you can add 2 templates. They helps the user to create feeds, configure them, ... automatically.
+For further details about that, please see https://github.com/emoncms/emoncms and https://github.com/emoncms/device
+```sh
+# deploy the template with general data
+cp EnvoyS.json /var/www/emoncms/Modules/device/data/EnvoyS.json
+
+# deploy the template with detailed data (general + each phase)
+cp EnvoyS-3phase.json /var/www/emoncms/Modules/device/data/EnvoyS-3phase.json
+```
+After that, you've to update the template list, just call:
+```
+http://my_emoncms/device/template/reload.json
+```
+That's it!
+Have fun!
